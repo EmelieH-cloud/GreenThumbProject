13f832fca4e2930fbadde5265cb26ecc87a75662
emeliehensfelt <emelie046@gmail.com>
2023-12-05 09:42:26 +0100

    Har skapat alla repositories + en unit of work-klass

---
+++ Data/GreenThumbUOW.cs
@@ -0,0 +1,28 @@
+﻿using GreenThumbProject.Models;
+
+namespace GreenThumbProject.Data
+{
+    public class GreenThumbUOW
+    {
+        public GardenRepository<Garden> GardenRepository { get; }
+        public PlantRepository<Plant> PlantRepository { get; }
+        public UserRepository<User> UserRepository { get; }
+        public InstructionRepository<Instruction> InstructionRepository { get; }
+
+        private readonly MyDBContext _DBcontext;
+
+        public GreenThumbUOW(MyDBContext context)
+        {
+            _DBcontext = context;
+            UserRepository = new(context);
+            PlantRepository = new(context);
+            InstructionRepository = new(context);
+            GardenRepository = new(context);
+        }
+
+        public async Task Complete()
+        {
+            await _DBcontext.SaveChangesAsync();
+        }
+    }
+}
